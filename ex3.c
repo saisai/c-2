dac8063b323308cb075f03c7e3c98b0dceeefa7a
buddhamagnet <buddhamagnet@gmail.com>
2013-07-08 18:41:27 +0100

    outine printf escape codes and format sequences

--- ex3.c
+++ ex3.c
@@ -9,3 +9,42 @@ int main() {
 
   return 0;
 }
+
+/*
+
+ESCAPE CODES:
+
+\a bell
+\b backspace
+\t horizontal tab
+\n newline
+\v vertical tab
+\f form feed
+\r carriage return
+\" double quote
+\' single quote
+\\ backslash
+\? question mark
+
+FORMAT SEQUENCES:
+
+%c character
+%d integer
+%e float, scientific notation
+%f float, six digits of precision
+%g float, up to six digits of precision
+%ld long integer
+%le double, scientifc notation
+%lf double, six digits of precision
+%lg double, up to six digits of precision
+%lu unsigned long
+%o octal number
+%s string
+%u unsigned
+%x hexadecimal number
+%X hexadecimal number
+
+
+EXTRA CREDIT
+
+*/
