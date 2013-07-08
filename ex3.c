ba5c8c06e87dcf2bcab0bb77f3c67580368d0ac8
buddhamagnet <buddhamagnet@gmail.com>
2013-07-08 19:50:30 +0100

    ex3 down

--- ex3.c
+++ ex3.c
@@ -47,4 +47,24 @@ FORMAT SEQUENCES:
 
 EXTRA CREDIT
 
+1. Find as many other ways to break ex3.c as you can.
+
+Done.
+
+2. Run man 3 printf and read about the other '%' format characters you can use.
+   These should look familiar if you used them in other languages (printf is
+   where they come from).
+
+Format sequences included above.
+
+3. Add ex3 to your Makefile's all list. Use this to make clean all and build
+  all your exercises so far.
+
+The Makefile is already pretty sweet.
+
+4. Add ex3 to your Makefile's clean list as well. Now use make clean will
+   remove it when you need to.
+
+As I said, my Makefile is better than that.
+
 */
