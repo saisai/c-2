463c5d31a64c3a6073e55ce36fd5da3098ca316f
buddhamagnet <buddhamagnet@gmail.com>
2013-07-09 20:49:41 +0100

    ex4 down

---
+++ ex4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main() {
+  int age = 10;
+  int height;
+
+  printf("I am %d years old.\n");
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
+
+/*
+
+EXTRA CREDIT
+
+1. Fix this program using Valgrind and the compiler as your guide.
+
+Valgrind is fucked on Mountain Lion.
+
+2. Read up on Valgrind on the internet.
+
+Fascinating,
+
+3. Download other software and build it by hand. 
+Try something you already use but never built for yourself.
+
+Bitch, please.
+
+4. Look at how the Valgrind source files are laid out in the 
+source directory and read its Makefile. Don't worry, none of 
+that makes sense to me either.
+
+Moon.
+
+*/
