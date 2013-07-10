5c07837ffd40192af4bb5d475601f578be3c5f52
buddhamagnet <buddhamagnet@gmail.com>
2013-07-10 19:42:37 +0100

    ex5 down

---
+++ ex5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[]) {
+  int distance = 10;
+
+  // this is also a comment
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
+
+/*
+
+EXTRA CREDIT:
+
+1. For each line, write out the symbols you don't understand and see 
+if you can guess what they mean. Write a little chart on paper with your 
+guess that you can use to check later and see if you get it right.
+
+I know all this shizzle.
+
+2. Go back to the source code from the previous exercises and do a 
+similar break-down to see if you're getting it. Write down what 
+you don't know and can't explain to yourself.
+
+I know all that shizzle.
+
+*/
