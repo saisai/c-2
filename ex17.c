4dd439c129d5a3666892711935e619da2327f436
buddhamagnet <buddhamagnet@gmail.com>
2013-07-22 09:43:08 +0100

    expanding ex17

--- ex17.c
+++ ex17.c
@@ -13,11 +13,11 @@ typedef struct {
   int set;
   char name[MAX_DATA];
   char email[MAX_DATA];
+  char postcode[MAX_DATA];
 } address;
 
 typedef struct {
-	int max_rows;
-  address rows[];
+  address rows[MAX_ROWS];
 } db;
 
 typedef struct {
@@ -41,19 +41,21 @@ void die(const char *message) {
 	exit(1);
 }
 
-address *address_create(int id, char *name, char *email) {
+address *address_create(int id, char *name, char *email, char *postcode) {
+
 	address *addr = malloc(sizeof(address));
 
 	addr->id = id;
 	addr->set = 0;
 	memcpy(addr->name, name, strlen(name));
 	memcpy(addr->email, email, strlen(email));
+	memcpy(addr->postcode, postcode, strlen(postcode));
 
 	return addr;
 }
 
 void address_print(address *addr) {
-	printf("%d %s (%s)\n", addr->id, addr->name, addr->email);
+	printf("%d %s (%s) resident at %s\n", addr->id, addr->name, addr->email, addr->postcode);
 }
 
 void address_destroy(address *addr) {
@@ -109,21 +111,23 @@ void db_write(c) {
 	}
 }
 
-void db_create(int max_rows) {
+void db_create() {
   int i = 0;
-  conn->db->max_rows = max_rows;
-  for (i = 0; i < conn->db->max_rows; i++) {
+  for (i = 0; i < MAX_ROWS; i++) {
   	address addr = {.id = i, .set = 0};
   	conn->db->rows[i] = addr;
   }
 }
 
-void db_set(int id, const char *name, const char *email) {
+void db_set(int id, const char *name, const char *email, const char *postcode) {
+
 	address *addr = &conn->db->rows[id];
 	if (addr->set) {
 		die("Already set, delete it first.");
 	}
+
   addr->set = 1;
+
   char *res = strncpy(addr->name, name, MAX_DATA);
   if (!res) {
   	die("Failed to copy name.");
@@ -131,6 +135,10 @@ void db_set(int id, const char *name, const char *email) {
   res = strncpy(addr->email, email, MAX_DATA);
   if (!res) {
   	die("Failed to copy email.");
+  }
+  res = strncpy(addr->postcode, postcode, MAX_DATA);
+  if (!res) {
+  	die("Failed to copy postcode.");
   }
 }
 
@@ -149,6 +157,22 @@ void db_delete(int id) {
 	conn->db->rows[id] = addr;
 }
 
+void db_find(char *text) {
+	int i = 0;
+	int found = 0;
+	db *db = conn->db;
+	for (i = 0; i < MAX_ROWS; i++) {
+		address *current = &db->rows[i];
+		if (strcmp(text, current->name) == 0 || strcmp(text, current->email) == 0 || strcmp(text, current->postcode) == 0) {
+			address_print(current);
+			found = 1;
+		}
+	}
+	if (found == 0) {
+		printf("No matching records found.\n");
+	}
+}
+
 void db_list() {
 	int i = 0;
 	db *db = conn->db;
@@ -163,7 +187,7 @@ void db_list() {
 void db_iterate() {
 	int i;
 	// Demonstrate addresses of database rows.
-	for (i = 0; i < conn->db->max_rows; i++) {
+	for (i = 0; i < MAX_ROWS; i++) {
 	  printf("Address of record at row %d: %p.\n", i + 1, &conn->db->rows[i]);
 		usleep(10000);
 	}
@@ -174,23 +198,10 @@ int main(int argc, char *argv[]) {
 	if (argc < 3) die("USAGE: ex17 <dbfile> <action> [params]");
 
 	int i;
-
-	  /* Exercise address creation, display and deletion.
-	  address *addr = address_create(100, "buddhamagnet", "buddhamagnet@gmail.com");
-	  address_print(addr);
-	  address_destroy(addr);
-
-	  // Demonstrate address for connection.
-	  connection *c = db_open("db.dat", 'c');
-	  printf("Just created a connection at address %p.\n", c);
-	  printf("The connection's database is stored at address: %p.\n", c->db);
-	  printf("The connection's database contains %ld rows (addresses).\n", sizeof(c->db->rows) / sizeof(address));
-
-	  */
-
+	int id = 0;
 	char *filename = argv[1];
 	char action = argv[2][0];
-	int id = 0;
+
 	db_open(filename, action);
 
 	if (argv[2][0] != 'c' && argc > 3) id = atoi(argv[3]);
@@ -198,8 +209,7 @@ int main(int argc, char *argv[]) {
 
 	switch (action) {
 		case 'c':
-		if (argc != 4) die("Please supply a max rows value for the database.");
-		  db_create(atoi(argv[3]));
+		  db_create();
 		  db_write();
 		  break;
 		case 'g':
@@ -209,9 +219,13 @@ int main(int argc, char *argv[]) {
 		case 'i':
 		  db_iterate();
 		  break;
+		case 'f':
+		  if (argc != 4) die("Please supply a name to search on.");
+		  db_find(argv[3]);
+		  break;
 		case 's':
-		  if (argc != 6) die("Please supply an ID, name and email");
-		  db_set(id, argv[4], argv[5]);
+		  if (argc != 7) die("Please supply an ID, name, email and postcode");
+		  db_set(id, argv[4], argv[5], argv[6]);
 		  db_write();
 		  break;
 		case 'd':
@@ -254,6 +268,8 @@ in the db struct, and write that to the file, thus creating a database
 that can be arbitrarily sized.
 
 3. Add more operations you can do on the database, like find.
+
+Rudimentary find function added.
 
 4. Read about how C does it's struct packing, and then try to see why your file
 is the size it is. See if you can calculate a new size after adding more fields.
