ebd6d6132ba16e442c1088c71968c160522e4d81
buddhamagnet <buddhamagnet@gmail.com>
2013-07-23 09:11:51 +0000

    ex18 almost down

--- ex18.c
+++ ex18.c
@@ -53,6 +53,9 @@ int strange_order(int a, int b) {
   }
 }
 
+void invalid_function() {
+}
+
 void test_sort(int *numbers, int count, compare_cb cmp) {
   int i = 0;
   int *sorted = bubble_sort(numbers, count, cmp);
@@ -62,6 +65,15 @@ void test_sort(int *numbers, int count, compare_cb cmp) {
   }
   printf("\n");
   free(sorted);
+}
+
+void dump_callback(compare_cb cmp) {
+  unsigned char *data = (unsigned char *)cmp;
+  int i;
+  for (i = 0; i < 25; i++) {
+    printf("%02x:", data[i]);
+  }
+  printf("\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -81,8 +93,47 @@ int main(int argc, char *argv[]) {
   test_sort(numbers, count, sorted_order);
   test_sort(numbers, count, reverse_order);
   test_sort(numbers, count, strange_order);
+  test_sort(numbers, count, invalid_function);
+  dump_callback(sorted_order);
+  dump_callback(reverse_order);
+  dump_callback(strange_order);
 
   free(numbers);
 
 	return 0;
 }
+
+/*
+
+HOW TO BREAK IT:
+
+Instead of adding Zed's crazy function dump at the end of test_sorted, I decided to keep
+that function clean and add a new function that takes a compare_cb and dumps it.
+
+EXTRA CREDIT:
+
+1. Get a hex editor and open up ex18, then find this sequence of hex digits that start a function to
+see if you can find the function in the raw program.
+
+Yep, as indicated by the results of dump_callback.
+
+2. Find other random things in your hex editor and change them. Rerun your program and see what happens.
+Changing strings you find are the easiest things to change.
+
+The usual chaos.
+
+3. Pass in the wrong function for the compare_cb and see what the C compiler complains about.
+
+I pass in invalid_function above and get: warning: passing argument 3 of ‘test_sort’ from incompatible pointer type.
+
+The program still runs and the numbers get passed through with no change.
+
+4. Pass in NULL and watch your program seriously bite it. Then run Valgrind and see what that reports.
+
+Segmentation fault, as expected.
+
+5. Write another sorting algorithm, then change test_sorting so that it takes both an arbitrary
+sort function and the sort function's callback comparison. Use it to test both of your algorithms.
+
+*/
+
